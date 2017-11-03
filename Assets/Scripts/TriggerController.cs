e20c9eeb6908c0ddaed7c03d03e2bc81f58338b8
Evan Kiser <ekiser247@gmail.com>
2017-11-02 21:37:22 -0500

    Initial Commit

---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+    public GameObject animObject;
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = animObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Player"))
+        {
+            anim.SetTrigger("Triggered");
+        }
+    }
+}
