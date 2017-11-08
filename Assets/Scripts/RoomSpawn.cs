5c184eb510d219a29dbfe97eeddc4a46f079ddee
Evan Kiser <ekiser247@gmail.com>
2017-11-07 22:24:24 -0600

    Added some stuff and modified stuff

--- Assets/Scripts/RoomSpawn.cs
+++ Assets/Scripts/RoomSpawn.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using System.Globalization;
+//using System;
+//using UnityEngine;
 
-[System.Serializable]
-public class LevelController
-{
-    public GameObject[] Room;
-}
+//[System.Serializable]
+//public class LevelController
+//{
+//    public GameObject[] Room;
+//}
 
-public class RoomSpawn : MonoBehaviour {
+//public class RoomSpawn : MonoBehaviour {
 
-    public LevelController[] Level;
-}
+//    public LevelController[] Level;
+//    public GameObject[] Rooms;
+
+//    private void Start()
+//    {
+//        for(int i = 0; i < LevelController.Room.Length; )
+//    }
+//}
