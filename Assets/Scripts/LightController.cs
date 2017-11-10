08b59880cd0a4e878b482249290e33f89bf66b60
Evan Kiser <ekiser247@gmail.com>
2017-11-09 21:35:14 -0600

    made the lights flicker randomly

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    public GameObject animObject;
+    public Animator anim;
+    Light light;
+
+
+    private void Start()
+    {
+        anim = animObject.GetComponent<Animator>();
+        light = animObject.GetComponentInChildren<Light>();
+        StartCoroutine("Flicker");
+    }
+
+    private void Update()
+    {
+        if (light.enabled == true)
+        {
+            anim.SetBool("LightOn", true);
+        }
+        else
+        {
+            anim.SetBool("LightOn", false);
+        }
+    }
+
+    IEnumerator Flicker()
+    {
+        while (enabled)
+        {
+            int num = Random.Range(0, 10);
+            if (num == 3)
+            {
+                anim.SetTrigger("Flicker");
+            }
+            yield return new WaitForSecondsRealtime(10);
+        }
+    }
+}
