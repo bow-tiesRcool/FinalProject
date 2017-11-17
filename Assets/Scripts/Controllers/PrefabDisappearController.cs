6f31afd653c8918cf8d340c23345436d93cefaa0
Evan Kiser <ekiser247@gmail.com>
2017-11-16 18:45:28 -0600

    Created more Hallways and made them destroy when entering next room

---
+++ Assets/Scripts/Controllers/PrefabDisappearController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDisappearController : MonoBehaviour {
+
+	public GameObject hallway;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.CompareTag("Player");
+		Destroy(hallway);
+	}
+}
