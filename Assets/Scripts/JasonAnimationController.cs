8c2616921c503d36d515ac53edc1d5b9c44042c4
Evan Kiser <ekiser247@gmail.com>
2017-11-30 19:13:01 -0600

    Reverted back before I merged with Chris

---
+++ Assets/Scripts/JasonAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JasonAnimationController : MonoBehaviour {
+    Animator anim;
+    //use these when your changing the animator's peramitors
+
+    //change the animators forward peramitors to make these things happen
+    float walk = .6f;
+    float run = 3f;
+    float idle = 0f;
+
+    //change the animators side peramitors to make these happen
+    float turnLeft = -1f;
+    float turnRight = 1f;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start () {
+        anim.SetFloat("Forward", 2f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
