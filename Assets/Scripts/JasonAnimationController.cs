82d568eae20d8204fa4d16bef9c6d984aa959a76
Evan Kiser <ekiser247@gmail.com>
2017-11-30 22:40:07 -0600

    Created a Set of instructions for Jason

--- Assets/Scripts/JasonAnimationController.cs
+++ Assets/Scripts/JasonAnimationController.cs
@@ -2,31 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class EnemyController
+{
+	public bool running;
+	public bool stop;
+	public float timeTillNext;
+	public bool disappear;
+}
+
+
 public class JasonAnimationController : MonoBehaviour {
     Animator anim;
     //use these when your changing the animator's peramitors
 
     //change the animators forward peramitors to make these things happen
-    float walk = .6f;
-    float run = 3f;
+    float walk = .5f;
+    float run = 1f;
     float idle = 0f;
-
+	float speed;
+	public Collider trigger;
     //change the animators side peramitors to make these happen
     float turnLeft = -1f;
     float turnRight = 1f;
+	public EnemyController[] setOfInstructions;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+		trigger = GetComponentInChildren<Collider>();
     }
 
-    // Use this for initialization
-    void Start () {
-        anim.SetFloat("Forward", 2f);
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			Debug.Log(other);
+			StartCoroutine("Controller");
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	IEnumerator Controller()
+	{
+		for (int i = 0; i < setOfInstructions.Length; i++)
+		{
+			if (setOfInstructions[i].running)
+			{
+				speed = run;
+			}
+			else
+			{
+				speed = walk;
+			}
+			if (setOfInstructions[i].stop)
+			{
+				speed = idle;
+			}
+			if (setOfInstructions[i].disappear)
+			{
+				gameObject.SetActive(false);
+			}
+
+			anim.SetFloat("Forward", speed);
+			yield return new WaitForSeconds(setOfInstructions[i].timeTillNext);
+		}
 		
 	}
 }
